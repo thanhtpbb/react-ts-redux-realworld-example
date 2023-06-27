0c2445b1c6209020a20e13d0d057f314102e9e56
thanhtran17 <enigma15012001@gmail.com>
2023-06-27 22:01:42 +0700

    edit article

--- src/actions/article.ts
+++ src/actions/article.ts
@@ -1,7 +1,18 @@
 import { apiCall } from '@/configs/api'
 import { API_URLS } from '@/configs/api/endpoint'
-import { UpdateArticlePayload } from '@/configs/api/payload'
+import { CreateArticlePayload, UpdateArticlePayload } from '@/configs/api/payload'
 import { Callback } from '@/types/others/callback'
+
+const createArticle = async (payload: CreateArticlePayload, cb?: Callback) => {
+  const { response, error } = await apiCall({ ...API_URLS.ARTICLES.POST_ARTICLE(), payload })
+
+  if (!error && response?.status === 200) {
+    const result = response.data
+    cb?.onSuccess?.(result.article)
+  } else {
+    cb?.onError?.()
+  }
+}
 
 const updateArticle = async (slug: string, payload: UpdateArticlePayload, cb?: Callback) => {
   const { response, error } = await apiCall({
@@ -25,4 +36,15 @@ const deleteArticle = async (slug: string, cb?: Callback) => {
   }
 }
 
-export const articleActions = { deleteArticle, updateArticle }
+const getArticle = async (slug: string, cb?: Callback) => {
+  const { response, error } = await apiCall({ ...API_URLS.ARTICLES.GET_ARTICLE(slug) })
+
+  if (!error && response?.status === 200) {
+    const result = response.data
+    cb?.onSuccess?.(result.article)
+  } else {
+    cb?.onError?.()
+  }
+}
+
+export const articleActions = { deleteArticle, updateArticle, createArticle, getArticle }
--- src/pages/Editor/components/EditorForm.tsx
+++ src/pages/Editor/components/EditorForm.tsx
@@ -1,19 +1,35 @@
+import { articleActions } from '@/actions/article'
 import { ROUTER } from '@/configs/router'
-import { useAppDispatch } from '@/hooks/redux'
-import { articleActions } from '@/redux/reducers/article/article.action'
 import { IArticle } from '@/types/models/IArticle'
 import { trimStringToArray } from '@/utils/helper'
-import { FormEvent, useRef } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { FormEvent, useEffect, useRef, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EditorForm = () => {
-  const dispatch = useAppDispatch()
+  const { slug } = useParams()
   const navigate = useNavigate()
+
+  const [currentArticle, setCurrentArticle] = useState<IArticle>()
 
   const titleRef = useRef<HTMLInputElement>(null)
   const descriptionRef = useRef<HTMLInputElement>(null)
   const bodyRef = useRef<HTMLTextAreaElement>(null)
   const tagsRef = useRef<HTMLInputElement>(null)
+
+  const navigateToArticleDetail = (article: IArticle) => {
+    navigate(`${ROUTER.ARTICLE_BASE}/${article.slug}`)
+  }
+
+  const createNewArticle = (title: string, description: string, body: string, tagList: string[]) => {
+    const article = { body, description, title, tagList }
+    articleActions.createArticle({ article }, { onSuccess: navigateToArticleDetail })
+  }
+
+  const updateArticle = (title: string, description: string, body: string) => {
+    if (!slug) return
+    const article = { body, description, title }
+    articleActions.updateArticle(slug, { article }, { onSuccess: navigateToArticleDetail })
+  }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -22,39 +38,48 @@ const EditorForm = () => {
     const description = descriptionRef.current?.value || ''
     const body = bodyRef.current?.value || ''
     const tags = tagsRef.current?.value || ''
-
     const tagList = trimStringToArray(tags)
 
-    dispatch(
-      articleActions.createArticle(
-        {
-          article: {
-            body,
-            description,
-            title,
-            tagList,
-          },
-        },
-        {
-          onSuccess: (result: IArticle) => {
-            navigate(`${ROUTER.ARTICLE_BASE}/${result.slug}`)
-          },
-        }
-      )
-    )
+    if (!slug) {
+      createNewArticle(title, description, body, tagList)
+      return
+    }
+    updateArticle(title, description, body)
   }
+
+  useEffect(() => {
+    if (!slug) return
+    articleActions.getArticle(slug, {
+      onSuccess: setCurrentArticle,
+    })
+  }, [])
+
+  const { title, description, body, tagList } = currentArticle || {}
 
   return (
     <form onSubmit={handleSubmit}>
       <fieldset>
         <fieldset className="form-group">
-          <input ref={titleRef} type="text" className="form-control form-control-lg" placeholder="Article Title" />
+          <input
+            ref={titleRef}
+            defaultValue={title}
+            type="text"
+            className="form-control form-control-lg"
+            placeholder="Article Title"
+          />
         </fieldset>
         <fieldset className="form-group">
-          <input ref={descriptionRef} type="text" className="form-control" placeholder="What's this article about?" />
+          <input
+            defaultValue={description}
+            ref={descriptionRef}
+            type="text"
+            className="form-control"
+            placeholder="What's this article about?"
+          />
         </fieldset>
         <fieldset className="form-group">
           <textarea
+            defaultValue={body}
             ref={bodyRef}
             className="form-control"
             rows={8}
@@ -62,7 +87,13 @@ const EditorForm = () => {
           ></textarea>
         </fieldset>
         <fieldset className="form-group">
-          <input ref={tagsRef} type="text" className="form-control" placeholder="Enter tags" />
+          <input
+            defaultValue={tagList?.join(', ')}
+            ref={tagsRef}
+            type="text"
+            className="form-control"
+            placeholder="Enter tags"
+          />
           <div className="tag-list"></div>
         </fieldset>
         <button className="btn btn-lg pull-xs-right btn-primary" type="submit">
